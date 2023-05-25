ddb8186e4280d8946283245e80356ab8b3a356e9
Alexander Bensberg <abensberg@astrophysik.uni-kiel.de>
2023-05-25 11:04:46 +0200

    Final fix of st.cache

--- vlti_b-var.py
+++ vlti_b-var.py
@@ -18,6 +18,7 @@ from scipy import interpolate
 mpl.rcParams.update({'lines.linewidth': 2})
 mpl.rcParams['lines.markersize'] = 3
 
+@st.cache_data
 def fft(imap, pixelsize, u, v, wave):
     
     fft=np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(imap)))
@@ -29,7 +30,6 @@ def fft(imap, pixelsize, u, v, wave):
     
     return real+imag*1j
 
-@st.cache_data
 def get_rise_and_set_HA(declination = -24*u.deg, 
                         obs_lat = -24.63*u.deg, 
                         obs_long = -70.4*u.deg, 
@@ -236,6 +236,7 @@ def get_uv(DEC_deg, HA_hour, use_telescopes, phi_deg=-24.6279483, quiet=True):
 
     return uvw_coords[:,:,:2], BL, PA_deg, alt_deg
 
+@st.cache_data
 def calc_vis(imap, u_m, v_m, wave_m, pixelsize_rad):
     uv_shape = u_m.shape
     u_m = u_m.flatten()
